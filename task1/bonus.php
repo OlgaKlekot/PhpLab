60ef062e12ee44539992373998cc9d5c0b96b1b3
Оля <klekot.olga@gmail.com>
2017-07-27 22:20:52 +0300

    bonus task

---
+++ task1/bonus.php
@@ -0,0 +1,33 @@
+<?php
+$eq = function ($str1, $str2) {
+    $strCount = function ($str) {
+        $sum = 0;
+        if (($str == null) || ($str == !ctype_alpha($str))) {
+            $sum = 0;
+        } else {
+            $str = str_split(strtoupper($str));
+            for ($i = 0; $i < count($str); $i++) {
+                $str_x = ord($str[$i]);
+                $sum += $str_x;
+            };
+        };
+        return $sum;
+    };
+    if ($strCount($str1) == $strCount($str2)) {
+        echo 'equal';
+    } else {
+        echo 'not equal';
+    };
+};
+
+$eq('AD', 'DD');
+echo '<br />';
+$eq('gf', 'FG');
+echo '<br />';
+$eq('zz1', '');
+echo '<br />';
+$eq('ZzZz', 'ffPFF');
+echo '<br />';
+$eq('kl', 'lz');
+echo '<br />';
+$eq(null, '');
